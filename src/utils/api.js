f5c7698402494ebb3a73189bf95b80ffd7b8d6db
pirabaasegar <pirabaa31@gmail.com>
2024-09-19 22:12:22 -0400

    ---

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const fetchUserSubscriptions = async (token) => {
   try {
-    // Fetch the user's subscriptions
     const subscriptionsResponse = await axios.get('https://www.googleapis.com/youtube/v3/subscriptions', {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -13,16 +12,17 @@ export const fetchUserSubscriptions = async (token) => {
         mine: true,
       },
     });
-
-    // Process the response data
+  
+    console.log('Subscriptions Response:', subscriptionsResponse.data); // Log the full response
+  
     const subscriptions = subscriptionsResponse.data.items.map(item => ({
       title: item.snippet.title,
       channelId: item.snippet.resourceId.channelId,
     }));
-
-    return subscriptions;
+  
+    return { subscriptions };
   } catch (error) {
-    console.error('Error fetching user subscriptions:', error);
-    return [];
+    console.error('Error fetching user subscriptions:', error.response ? error.response.data : error.message);
+    return { subscriptions: [] };
   }
 };
