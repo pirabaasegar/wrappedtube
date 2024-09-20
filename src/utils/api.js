3d0923d6baf25ce13e8ce10cb02d55796294cb10
pirabaasegar <pirabaa31@gmail.com>
2024-09-19 21:59:48 -0400

    ---

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,11 @@
 import axios from 'axios';
 
 export const fetchYouTubeData = async (token) => {
-  try {
-    const response = await axios.get('https://www.googleapis.com/youtube/v3/channels?part=contentDetails&mine=true', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    console.log(response.data);
-  } catch (error) {
-    console.error('Error fetching YouTube data:', error);
-  }
+  const response = await axios.get('https://www.googleapis.com/youtube/v3/channels?part=contentDetails&mine=true', {
+    headers: {
+      Authorization: `Bearer ${token}`, // Use Bearer token for authorization
+    },
+  });
+
+  return response.data;
 };
