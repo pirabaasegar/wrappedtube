5f590a9f357122a4a3c3f1ba8e0d0470d299a568
pirabaasegar <pirabaa31@gmail.com>
2024-09-20 16:58:17 -0400

    ---

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,7 +20,7 @@ const Login = () => {
     <div className='d-flex flex-column justify-content-center align-items-center vh-100'>
       <div className='rounded-2 shadow bg-white p-5 border border-gray d-flex flex-column align-items-center'>
         <div className='d-flex flex-column align-items-center'>
-          <h1 className='fs-3 fw-semi'>Login with Google</h1>
+          <h1 className='fs-3 fw-semi'>See your YouTube Wrapped now</h1>
           <p className='fs-5 mt-3'>Your top most watched videos, watchtime all in one place</p>
           <GoogleLogin
             onSuccess={handleLoginSuccess}
