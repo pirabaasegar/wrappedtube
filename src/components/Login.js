5c5e58620827e3ce878bbaa1c4f39cb774e5673b
pirabaasegar <pirabaa31@gmail.com>
2024-09-20 16:57:44 -0400

    ---

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
   return (
     <div className='d-flex flex-column justify-content-center align-items-center vh-100'>
       <div className='rounded-2 shadow bg-white p-5 border border-gray d-flex flex-column align-items-center'>
-        <div className='text-center px-5'>
+        <div className='d-flex flex-column align-items-center'>
           <h1 className='fs-3 fw-semi'>Login with Google</h1>
           <p className='fs-5 mt-3'>Your top most watched videos, watchtime all in one place</p>
           <GoogleLogin
