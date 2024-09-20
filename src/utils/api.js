5ada5901009ac10768154691c84000c69649f373
pirabaasegar <pirabaa31@gmail.com>
2024-09-19 20:30:55 -0400

    ---

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const fetchYouTubeData = async (token) => {
   const response = await axios.get('https://www.googleapis.com/youtube/v3/channels?part=contentDetails&mine=true', {
     headers: {
-      Authorization: `Bearer ${token}`,
+      'Authorization': `Bearer ${token}`
     },
   });
 
