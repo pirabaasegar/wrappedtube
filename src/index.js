69cf132ff8eb5487369666896f0b4817a6badfec
pirabaasegar <pirabaa31@gmail.com>
2024-09-19 20:57:08 -0400

    ---

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './components/App';
 import './App.css';
 
-const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+const clientId = process.env.GOOGLE_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
