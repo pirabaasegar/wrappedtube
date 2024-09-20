8d2b105e828e63a8a078f8ffdab2e69afc5567bb
pirabaasegar <pirabaa31@gmail.com>
2024-09-20 17:17:19 -0400

    ---

--- src/components/Login.js
+++ src/components/Login.js
@@ -25,7 +25,7 @@ const Login = () => {
         <div className='d-flex flex-column align-items-center'>
           <h1 className='fs-3 fw-semi m-0'>See your <span className='fw-bold' style={{ color: 'rgb(255, 0, 0)' }}>YouTube</span> Wrapped now</h1>
           <p className='fs-5 m-0 mb-3'>Your top most watched videos, watchtime all in one place</p>
-          <button onClick={login} className='bg-white rounded-5 pe-auto border border-gray px-3 py-2 text-white' style={{ color: 'rgb(255, 0, 0)' }}>
+          <button onClick={login} className='rounded-5 pe-auto border border-gray px-3 py-2 text-white' style={{ color: 'rgb(255, 0, 0)' }}>
           <i class="bi bi-google me-1"></i>Sign in with Google
           </button>
         </div>
