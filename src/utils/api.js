a46360528aea9b60b2793ff4172729c83fd607e2
pirabaasegar <pirabaa31@gmail.com>
2024-09-19 21:55:26 -0400

    ---

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+
 export const fetchYouTubeData = async (token) => {
-  const response = await axios.get('https://www.googleapis.com/youtube/v3/videos?part=snippet&mySubscribers=true', {
+  const response = await axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet&mySubscribers=true&key=${apiKey}`, {
     headers: {
       'Authorization': `Bearer ${token}`,
     },
