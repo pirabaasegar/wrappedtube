9db2d0615a4824248de0686d8b599cc7703f3c65
pirabaasegar <pirabaa31@gmail.com>
2024-09-20 16:55:26 -0400

    ---

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,8 +20,8 @@ const Login = () => {
     <div className='d-flex flex-column justify-content-center align-items-center vh-100'>
       <div className='rounded-2 shadow bg-white p-5 border border-gray d-flex flex-column align-items-center'>
         <div className='text-center px-5'>
-          <h1 className='fw-semi'>Login with Google</h1>
-          <p className='mt-3'>Your top most watched videos, watchtime all in one place</p>
+          <h1 className='fs-3 fw-semi'>Login with Google</h1>
+          <p className='fs-5 mt-3'>Your top most watched videos, watchtime all in one place</p>
           <GoogleLogin
             onSuccess={handleLoginSuccess}
             onFailure={handleLoginFailure}
