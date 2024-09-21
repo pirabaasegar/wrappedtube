de66aa09cc7d1e56a81f9bc12bc49e3817c2b891
pirabaasegar <pirabaa31@gmail.com>
2024-09-21 14:18:17 -0400

    ---

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import StatsPage from './components/StatsPage';
