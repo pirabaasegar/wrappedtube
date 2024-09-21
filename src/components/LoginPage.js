6bd7a1a786478372856403d8088a77118fabc8f4
pirabaasegar <pirabaa31@gmail.com>
2024-09-21 11:16:50 -0400

    ---

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -38,7 +38,7 @@ const LoginPage = ({ setAccessToken }) => {
     <div className='d-flex flex-column justify-content-center align-items-center vh-100'>
     <div className='d-flex flex-column align-items-center'>
         <h1 className='fs-3 fw-semi m-0'>See your <span className='fw-bold' style={{ color: 'rgb(255, 0, 0)' }}>YouTube</span> Wrapped now</h1>
-        <p className='fs-5 m-0 mb-3'>Your top most watched videos, watchtime<br />all in one place</p>
+        <p className='fs-5 m-0 mb-3 text-center'>Your top most watched videos, watchtime<br />all in one place</p>
         <button onClick={login} className='rounded-5 pe-auto border-0 px-3 py-2 text-white bg-danger'>
         <i className="bi bi-google me-2"></i>Sign in with Google
         </button>
