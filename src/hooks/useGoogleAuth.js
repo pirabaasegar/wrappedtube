a2fa1b26792cffa014ca9a573c7448c095d2f025
pirabaasegar <pirabaa31@gmail.com>
2024-09-21 10:34:12 -0400

    ---

--- src/hooks/useGoogleAuth.js
+++ src/hooks/useGoogleAuth.js
@@ -5,7 +5,7 @@ const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
 
-const useGoogleAuth = () => {
+const useGoogleAuth = (setAccessToken) => {
   useEffect(() => {
     const initClient = () => {
       gapi.client.init({
@@ -13,13 +13,32 @@ const useGoogleAuth = () => {
         clientId: CLIENT_ID,
         scope: SCOPES,
         discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
-      }).catch((err) => {
-        console.error('Error initializing Google API:', err);
+      }).then(() => {
+        const authInstance = gapi.auth2.getAuthInstance();
+
+        // Check if the user is already signed in
+        if (authInstance.isSignedIn.get()) {
+          const token = authInstance.currentUser.get().getAuthResponse().access_token;
+          localStorage.setItem('accessToken', token); // Save to localStorage
+          setAccessToken(token);
+        }
+
+        // Listen for sign-in state changes
+        authInstance.isSignedIn.listen((isSignedIn) => {
+          if (isSignedIn) {
+            const token = authInstance.currentUser.get().getAuthResponse().access_token;
+            localStorage.setItem('accessToken', token); // Save to localStorage
+            setAccessToken(token);
+          } else {
+            localStorage.removeItem('accessToken'); // Clear the token on sign out
+            setAccessToken(null);
+          }
+        });
       });
     };
 
     gapi.load('client:auth2', initClient);
-  }, []);
+  }, [setAccessToken]);
 };
 
 export default useGoogleAuth;
