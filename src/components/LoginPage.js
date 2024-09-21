37a45255e32e81bff619d90c7059ba966080ef20
pirabaasegar <pirabaa31@gmail.com>
2024-09-21 11:13:16 -0400

    ---

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -36,15 +36,13 @@ const LoginPage = ({ setAccessToken }) => {
 
   return (
     <div className='d-flex flex-column justify-content-center align-items-center vh-100'>
-      <div className='rounded-2 shadow bg-white p-5 border border-gray d-flex flex-column align-items-center'>
-        <div className='d-flex flex-column align-items-center'>
-          <h1 className='fs-3 fw-semi m-0'>See your <span className='fw-bold' style={{ color: 'rgb(255, 0, 0)' }}>YouTube</span> Wrapped now</h1>
-          <p className='fs-5 m-0 mb-3'>Your top most watched videos, watchtime all in one place</p>
-          <button onClick={login} className='rounded-5 pe-auto border-0 px-3 py-2 text-white bg-danger'>
-            <i className="bi bi-google me-2"></i>Sign in with Google
-          </button>
-        </div>
-      </div>
+    <div className='d-flex flex-column align-items-center'>
+        <h1 className='fs-3 fw-semi m-0'>See your <span className='fw-bold' style={{ color: 'rgb(255, 0, 0)' }}>YouTube</span> Wrapped now</h1>
+        <p className='fs-5 m-0 mb-3'>Your top most watched videos, watchtime<br />all in one place</p>
+        <button onClick={login} className='rounded-5 pe-auto border-0 px-3 py-2 text-white bg-danger'>
+        <i className="bi bi-google me-2"></i>Sign in with Google
+        </button>
+    </div>
     </div>
   );
 };
